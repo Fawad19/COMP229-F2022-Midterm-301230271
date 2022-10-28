dd1c135438786b1449f952671e69b1aba38c8036
Fawad19 <fzaheer4@my.centennialcollege.ca>
2022-10-28 14:30:59 -0400

    Initial Commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //URI: "mongodb://127.0.0.1/emp",
+  URI: "mongodb+srv://fawad:XkQKlxVWJiwyHszj@cluster0.8s64hmk.mongodb.net/Emp?retryWrites=true&w=majority",
+};
